94d7678b69e1e0dece5c8138af57d627c49bd439
JDMQXD <juandamartinez04@gmail.com>
2025-10-30 19:06:51 -0500

    Se realiza el JWT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { VehiculoComponent } from './vehiculo/vehiculo';
+import { LoginComponent } from './login/login';
 
 export const routes: Routes = [
-    {path: 'vehiculos', component: VehiculoComponent}
+    {path: 'vehiculos', component: VehiculoComponent},
+    {path: 'login', component: LoginComponent}
 ];
